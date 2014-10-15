6d61f53f83b5256f588693fa387a7f1c9f6eab27
minsu.cho@mail.utoronto.ca <minsu.cho@mail.utoronto.ca>
2014-10-15 11:45:00 -0400

    mininet

--- cs144_lab3/router/sr_router.c
+++ cs144_lab3/router/sr_router.c
@@ -174,6 +174,7 @@ void sr_handlepacket_arp(struct sr_instance* sr,
     	memcpy(tx_packet, tx_e_hdr, sizeof(sr_ethernet_hdr_t));
     	memcpy(tx_packet + sizeof(sr_ethernet_hdr_t), tx_arp_hdr, sizeof( sr_arp_hdr_t));
 
+    	print_addr_eth((uint8_t*)(tx_packet));
     	sr_send_packet(sr, ((uint8_t*)(tx_packet)), sizeof(sr_ethernet_hdr_t) + sizeof(sr_arp_hdr_t), rx_if->name);
 
     	free(tx_packet);
