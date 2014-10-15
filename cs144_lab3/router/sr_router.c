8105781dc3bc4cd14e3a455979e8527383b62fd4
minsu.cho@mail.utoronto.ca <minsu.cho@mail.utoronto.ca>
2014-10-15 16:35:59 -0400

    mininet

--- cs144_lab3/router/sr_router.c
+++ cs144_lab3/router/sr_router.c
@@ -134,8 +134,8 @@ void sr_handlepacket_arp(struct sr_instance* sr,
     	/*printf(" sender information : %x \n", arp_header->ar_sha);
     	print_addr_ip_int(arp_header->ar_sip);*/
 
-
-    	if(sr_arpcache_insert(&(sr->cache), *arp_header->ar_sha, arp_header->ar_sip) == NULL){
+    	print_hdr_arp((uint8_t *)arp_header);
+    	if(sr_arpcache_insert(&(sr->cache), arp_header->ar_sha, arp_header->ar_sip) == NULL){
     	    fprintf(stderr, "Failed on inserting the sender information : \n");
     	}else{
 			struct sr_arpentry *is_have = sr_arpcache_lookup(&(sr->cache), arp_header->ar_tip);
