eb026457add7bf049cc4809ff28dd65b5895a74b
minsu.cho@mail.utoronto.ca <minsu.cho@mail.utoronto.ca>
2014-10-15 12:08:20 -0400

    mininet

--- cs144_lab3/router/sr_vns_comm.c
+++ cs144_lab3/router/sr_vns_comm.c
@@ -570,6 +570,7 @@ int sr_send_packet(struct sr_instance* sr /* borrowed */,
                          unsigned int len,
                          const char* iface /* borrowed */)
 {
+	sr_print_if(sr_get_interface(sr, iface));
     c_packet_header *sr_pkt;
     unsigned int total_len =  len + (sizeof(c_packet_header));
 
