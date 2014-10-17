0becdf0713df1e93928153645fce501f835bf8ce
minsu.cho@mail.utoronto.ca <minsu.cho@mail.utoronto.ca>
2014-10-17 01:51:47 -0400

    mininet

--- cs144_lab3/router/sr_router.c
+++ cs144_lab3/router/sr_router.c
@@ -216,14 +216,12 @@ void sr_handlepacket_ip(struct sr_instance* sr,
 	sr_ethernet_hdr_t* eth_orig_header = (sr_ethernet_hdr_t*)packet;
 	sr_ip_hdr_t* ip_orig_header = ((sr_ip_hdr_t*)(packet + sizeof(sr_ethernet_hdr_t)));
 
-	if(ip_orig_header->ip_sum != cksum(ip_orig_header, ip_orig_header->ip_hl)) {
+	if(htons(ip_orig_header->ip_sum) != cksum(ip_orig_header, ip_orig_header->ip_hl)) {
 	        printf("!!! Invalid checksum. \n");
 	        return;
 	}
 
 	/* Check whether there exists the destination from the packet is in the route table.*/
-	ip_orig_header->ip_dst;
-
 	sr_print_routing_table(sr);
 	print_hdr_eth(eth_orig_header);
 	print_hdr_ip((sr_ip_hdr_t*)(packet + sizeof(sr_ethernet_hdr_t)));
