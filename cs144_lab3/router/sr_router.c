e2fa23e36b6b4043f7629345d78926d2332a3a26
minsu.cho@mail.utoronto.ca <minsu.cho@mail.utoronto.ca>
2014-10-17 01:58:23 -0400

    mininet

--- cs144_lab3/router/sr_router.c
+++ cs144_lab3/router/sr_router.c
@@ -219,7 +219,7 @@ void sr_handlepacket_ip(struct sr_instance* sr,
 	print_hdr_ip(ip_orig_header);
 
 	printf("you have this %u\n", htons(ip_orig_header->ip_sum));
-	printf("you get this %u\n", htons(cksum(ip_orig_header, ip_orig_header->ip_len)));
+	printf("you get this %u\n", htons(cksum(ip_orig_header, ip_orig_header->ip_len*4)));
 	if(htons(ip_orig_header->ip_sum) != cksum(ip_orig_header, ip_orig_header->ip_len)) {
 	        printf("!!! Invalid checksum. \n");
 	        return;
