f4f69763a8ecdefb2b87a0c21a8bad1e705d0860
minsu.cho@mail.utoronto.ca <minsu.cho@mail.utoronto.ca>
2014-10-18 12:52:59 -0400

    mininet

--- cs144_lab3/router/sr_message.h
+++ cs144_lab3/router/sr_message.h
@@ -18,6 +18,7 @@ void send_ip_packet(struct sr_instance*, uint8_t*, char*, uint16_t, uint16_t);
 void build_ether_header(uint8_t *_packet, uint8_t *addr, struct sr_if* interfaces, uint16_t protocol);
 void build_arp_header(uint8_t *, sr_arp_hdr_t* , struct sr_if* );
 void build_ip_header(uint8_t *, sr_ip_hdr_t* , struct sr_if* );
-void build_icmp_header(uint8_t *, uint8_t *, sr_ip_hdr_t *, sr_icmp_hdr_t* , struct sr_if* , uint8_t , uint8_t );
+void build_icmp_t3_header(uint8_t *, uint8_t *, sr_ip_hdr_t *, sr_icmp_hdr_t* , struct sr_if* , uint8_t , uint8_t );
+void build_icmp_header(uint8_t *, uint8_t , uint8_t );
 
 #endif /* SR_MESSAGE_H */
