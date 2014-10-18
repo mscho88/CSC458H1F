d0e1c343b2577f9fd0ad1a0e5aa06a52077a264f
minsu.cho@mail.utoronto.ca <minsu.cho@mail.utoronto.ca>
2014-10-18 07:11:39 -0400

    mininet

--- cs144_lab3/router/sr_message.c
+++ cs144_lab3/router/sr_message.c
@@ -39,8 +39,8 @@ void send_ip_packet(struct sr_instance* sr, uint8_t* packet, char* interface, ui
 	uint8_t *_packet = (uint8_t *) malloc(length);
 
 	sr_ethernet_hdr_t *eth_header = (sr_ethernet_hdr_t *)packet;
-	sr_ip_hdr_t *ip_header = (sr_ip_hdr_t *)packet + sizeof(sr_ethernet_hdr_t);
-	sr_icmp_t3_hdr_t *icmp_header = (sr_icmp_t3_hdr_t *)packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_icmp_t3_hdr_t);
+	sr_ip_hdr_t* ip_header = (sr_ip_hdr_t*)(packet + sizeof(sr_ethernet_hdr_t));
+	sr_icmp_t3_hdr_t *icmp_header = (sr_icmp_t3_hdr_t *)(packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_icmp_t3_hdr_t));
 
 	print_hdr_ip(ip_header);
 	build_ether_header(_packet, eth_header->ether_shost, interfaces);
