c246f6d85ef00b5f1613a68ef90871647eba1e71
minsu.cho@mail.utoronto.ca <minsu.cho@mail.utoronto.ca>
2014-10-18 06:42:03 -0400

    mininet

--- cs144_lab3/router/sr_message.c
+++ cs144_lab3/router/sr_message.c
@@ -10,6 +10,47 @@
 #include "sr_message.h"
 
 /*---------------------------------------------------------------------
+ * Method: send_arp_packet(struct sr_instance* sr, uint8_t* packet,
+ * 							unsigned int len, char* interface)
+ * Scope:  Local
+ *
+ * Send an ARP reply packet to the sender
+ *
+ *---------------------------------------------------------------------*/
+void send_arp_packet(struct sr_instance* sr, uint8_t* packet, unsigned int len, char* interface){
+	unsigned int length = sizeof(sr_ethernet_hdr_t) + sizeof(sr_arp_hdr_t);
+	struct sr_if *interfaces = sr_get_interface(sr, interface);
+	uint8_t* _packet = (uint8_t*)malloc(length);
+	sr_ethernet_hdr_t* eth_header = (sr_ethernet_hdr_t *)packet;
+	sr_arp_hdr_t* arp_header = ((sr_arp_hdr_t*)(packet + sizeof(sr_ethernet_hdr_t)));
+
+	/* Build ARP Packet */
+	build_ether_header(_packet, eth_header->ether_shost, interfaces);
+	build_arp_header(_packet + sizeof(sr_ethernet_hdr_t), arp_header, interfaces);
+
+	sr_send_packet(sr, (uint8_t*)_packet, length, interfaces->name);
+	free(_packet);
+}/* end send_arp_packet */
+
+void send_ip_packet(struct sr_instance* sr, uint8_t* packet, char* interface, uint8_t type, uint8_t code){
+	int length = sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t) + sizeof(sr_icmp_t3_hdr_t);
+	struct sr_if *interfaces = sr_get_interface(sr, interface);
+
+	uint8_t *_packet = (uint8_t *) malloc(length);
+
+	sr_ethernet_hdr_t *eth_header = (sr_ethernet_hdr_t *)packet;
+	sr_ip_hdr_t *ip_header = (sr_ip_hdr_t *)packet + sizeof(sr_ethernet_hdr_t);
+	sr_icmp_hdr_t *icmp_header = (sr_icmp_hdr_t *)packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t);
+
+	build_ether_header(_packet, interfaces->addr, interfaces);
+	build_ip_header(_packet + sizeof(sr_ethernet_hdr_t), ip_header, interfaces);
+	build_icmp_header(_packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t), packet, ip_header, icmp_header, interfaces, type, code);
+
+	sr_send_packet(sr, (uint8_t *)_packet, length, interfaces->name);
+	free(_packet);
+}
+
+/*---------------------------------------------------------------------
  * Method: sr_handlepacket_arp(struct sr_instance* sr,
  *      						uint8_t*  packet,
  *      						unsigned int len,
@@ -41,28 +82,7 @@ void build_icmp_header(uint8_t *_packet, sr_icmp_hdr_t* icmp_header, struct sr_i
 }*/
 
 
-/*---------------------------------------------------------------------
- * Method: send_arp_packet(struct sr_instance* sr, uint8_t* packet,
- * 							unsigned int len, char* interface)
- * Scope:  Local
- *
- * Send an ARP reply packet to the sender
- *
- *---------------------------------------------------------------------*/
-void send_arp_packet(struct sr_instance* sr, uint8_t* packet, unsigned int len, char* interface){
-	unsigned int length = sizeof(sr_ethernet_hdr_t) + sizeof(sr_arp_hdr_t);
-	struct sr_if *interfaces = sr_get_interface(sr, interface);
-	uint8_t* _packet = (uint8_t*)malloc(length);
-	sr_ethernet_hdr_t* eth_header = (sr_ethernet_hdr_t *)packet;
-	sr_arp_hdr_t* arp_header = ((sr_arp_hdr_t*)(packet + sizeof(sr_ethernet_hdr_t)));
 
-	/* Build ARP Packet */
-	build_ether_header(_packet, eth_header->ether_shost, interfaces);
-	build_arp_header(_packet + sizeof(sr_ethernet_hdr_t), arp_header, interfaces);
-
-	sr_send_packet(sr, (uint8_t*)_packet, length, interfaces->name);
-	free(_packet);
-}/* end send_arp_packet */
 
 /*---------------------------------------------------------------------
  * Method: build_ether_header(uint8_t *_packet,
@@ -101,24 +121,6 @@ void build_arp_header(uint8_t *_packet, sr_arp_hdr_t* arp_orig_header, struct sr
 	memcpy(arp_tmp_header->ar_tha, arp_orig_header->ar_sha, ETHER_ADDR_LEN);
 	arp_tmp_header->ar_tip = arp_orig_header->ar_sip;
 }/* end build_arp_header */
-
-
-void send_ip_packet(struct sr_instance* sr, uint8_t* packet, char* interface, uint8_t type, uint8_t code){
-	int length = sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t) + sizeof(sr_icmp_t3_hdr_t);
-	struct sr_if *interfaces = sr_get_interface(sr, interface);
-
-	uint8_t *_packet = (uint8_t *) malloc(length);
-
-	sr_ethernet_hdr_t *eth_header = (sr_ethernet_hdr_t *)packet;
-	sr_ip_hdr_t *ip_header = (sr_ip_hdr_t *)packet + sizeof(sr_ethernet_hdr_t);
-	sr_icmp_hdr_t *icmp_header = (sr_icmp_hdr_t *)packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t);
-
-	build_ether_header(_packet, interfaces->addr, interfaces);
-	build_ip_header(_packet + sizeof(sr_ethernet_hdr_t), ip_header, interfaces);
-	build_icmp_header(_packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t), packet, ip_header, icmp_header, interfaces, type, code);
-
-	sr_send_packet(sr, _packet, length, interface);
-}
 
 void build_icmp_header(uint8_t *_packet, uint8_t *packet, sr_ip_hdr_t *ip_header, sr_icmp_hdr_t* icmp_orig_header, struct sr_if* interfaces, uint8_t type, uint8_t code){
 	sr_icmp_t3_hdr_t *icmp_tmp_hdr = (sr_icmp_t3_hdr_t *)(_packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t));
