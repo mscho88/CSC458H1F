1a3c2796cb1e32c0932224f62361be070bd6d0e5
minsu.cho@mail.utoronto.ca <minsu.cho@mail.utoronto.ca>
2014-10-18 09:44:25 -0400

    mininet

--- cs144_lab3/router/sr_message.c
+++ cs144_lab3/router/sr_message.c
@@ -119,13 +119,12 @@ void build_ip_header(uint8_t *_packet, sr_ip_hdr_t* ip_header, struct sr_if* int
 
 	ip_tmp_header->ip_src = interfaces->ip;
 	ip_tmp_header->ip_dst = ip_header->ip_src;
-	ip_tmp_header->ip_len = ip_header->ip_len;
 	ip_tmp_header->ip_id = 0;
 	ip_tmp_header->ip_off = ip_header->ip_off;
 	ip_tmp_header->ip_ttl = ip_header->ip_ttl;
 	ip_tmp_header->ip_p = ip_protocol_icmp;
 	ip_tmp_header->ip_sum = 0;
-	ip_header->ip_sum = 0;
+	/*ip_header->ip_sum = 0;*/
 	ip_tmp_header->ip_sum = cksum(ip_tmp_header, sizeof(sr_ip_hdr_t));
 }
 
@@ -134,8 +133,7 @@ void build_icmp_header(uint8_t *_packet, uint8_t *packet, sr_ip_hdr_t *ip_header
 	icmp_tmp_hdr->icmp_type = type;
 	icmp_tmp_hdr->icmp_code = code;
 	memcpy(icmp_tmp_hdr->data, ip_header, 20);
-	memcpy(icmp_tmp_hdr->data + 20, packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t), 8);
+	memcpy(icmp_tmp_hdr->data + 20, icmp_orig_header, 8);
 	icmp_tmp_hdr->icmp_sum = 0;
-	icmp_tmp_hdr->icmp_sum = cksum(_packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t),
-							sizeof(sr_icmp_t3_hdr_t));
+	icmp_tmp_hdr->icmp_sum = cksum(icmp_tmp_hdr, sizeof(sr_icmp_t3_hdr_t));
 }
