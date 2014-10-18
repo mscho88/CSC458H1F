38a517dcc2c114f89c75d51e268fd119febf346a
minsu.cho@mail.utoronto.ca <minsu.cho@mail.utoronto.ca>
2014-10-18 14:48:19 -0400

    mininet

--- cs144_lab3/router/sr_router.c
+++ cs144_lab3/router/sr_router.c
@@ -279,7 +279,15 @@ void forward_packet(struct sr_instance *sr, char *interface, unsigned char *dest
 	ip_tmp_header->ip_sum = cksum(ip_tmp_header, sizeof(sr_ip_hdr_t));
 
 	print_hdr_ip(ip_tmp_header);
+	sr_icmp_hdr_t *icmp_header = (sr_icmp_hdr_t*)(packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t));
 
+	sr_icmp_t3_hdr_t *icmp_tmp_hdr = (sr_icmp_t3_hdr_t *)_packet;
+	icmp_tmp_hdr->icmp_type = icmp_header->icmp_type;
+	icmp_tmp_hdr->icmp_code = icmp_header->icmp_code;
+	icmp_tmp_hdr->icmp_sum = 0;
+	icmp_tmp_hdr->icmp_sum = cksum(icmp_tmp_hdr, sizeof(sr_icmp_hdr_t));
+
+	print_hdr_icmp(icmp_header);
 	sr_send_packet(sr, _packet, len, interface);
 	free(_packet);
 }
