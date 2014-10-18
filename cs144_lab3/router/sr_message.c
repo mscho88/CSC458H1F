5890b3887924dea8e897758bc4fa7d4a8268fba1
minsu.cho@mail.utoronto.ca <minsu.cho@mail.utoronto.ca>
2014-10-18 11:16:57 -0400

    mininet

--- cs144_lab3/router/sr_message.c
+++ cs144_lab3/router/sr_message.c
@@ -48,11 +48,7 @@ void send_ip_packet(struct sr_instance* sr, uint8_t* packet, char* interface, ui
 	printf("**********\n");
 	build_ether_header(_packet, eth_header->ether_shost, interfaces, ethertype_ip);
 	build_ip_header(_packet + sizeof(sr_ethernet_hdr_t), ip_header, interfaces);
-	((sr_ip_hdr_t *)(_packet + sizeof(sr_ethernet_hdr_t)))->ip_sum = 0;
-	((sr_ip_hdr_t *)(_packet + sizeof(sr_ethernet_hdr_t)))->ip_sum = cksum(_packet + sizeof(sr_ethernet_hdr_t), sizeof(sr_ip_hdr_t));
 	build_icmp_header(_packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t), packet, ip_header, icmp_header, interfaces, type, code);
-	((sr_icmp_t3_hdr_t *)(_packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t)))->icmp_sum = 0;
-	((sr_icmp_t3_hdr_t *)(_packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t)))->icmp_sum = cksum(_packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t), sizeof(sr_icmp_t3_hdr_t));
 
 	printf("***new***\n");
 	print_hdr_ip(_packet + sizeof(sr_ethernet_hdr_t));
@@ -130,17 +126,17 @@ void build_ip_header(uint8_t *_packet, sr_ip_hdr_t* ip_header, struct sr_if* int
 	ip_tmp_header->ip_id = 0;
 	ip_tmp_header->ip_off = 0;/*htons(ip_header->ip_off);*/
 	ip_tmp_header->ip_ttl = 64;/*ip_header->ip_ttl;*/
-	ip_tmp_header->ip_p = htons(ip_protocol_icmp);
-
+	ip_tmp_header->ip_p = htons(ip_protocol_arp);
+	ip_tmp_header->ip_sum = 0;
+	ip_tmp_header->ip_sum = cksum(ip_tmp_header, sizeof(sr_ip_hdr_t));
 }
 
 void build_icmp_header(uint8_t *_packet, uint8_t *packet, sr_ip_hdr_t *ip_header, sr_icmp_hdr_t* icmp_orig_header, struct sr_if* interfaces, uint8_t type, uint8_t code){
 	sr_icmp_t3_hdr_t *icmp_tmp_hdr = (sr_icmp_t3_hdr_t *)_packet;
 	icmp_tmp_hdr->icmp_type = type;
 	icmp_tmp_hdr->icmp_code = code;
-	icmp_tmp_hdr->icmp_sum = 0;
 	memcpy(icmp_tmp_hdr->data, ip_header, 20);
 	memcpy(icmp_tmp_hdr->data + 20, icmp_orig_header, 8);
-
-	/*icmp_tmp_hdr->icmp_sum = cksum(icmp_tmp_hdr, sizeof(sr_icmp_t3_hdr_t));*/
+	icmp_tmp_hdr->icmp_sum = 0;
+	icmp_tmp_hdr->icmp_sum = cksum(icmp_tmp_hdr, sizeof(sr_icmp_t3_hdr_t));
 }
