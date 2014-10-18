2cde334eb3147aa75acc5d50e2f7cf891c615082
minsu.cho@mail.utoronto.ca <minsu.cho@mail.utoronto.ca>
2014-10-18 13:35:31 -0400

    mininet

--- cs144_lab3/router/sr_packet.h
+++ cs144_lab3/router/sr_packet.h
@@ -13,16 +13,17 @@
 #include "sr_utils.h"
 
 static uint8_t Broadcast[ETHER_ADDR_LEN] = {255, 255, 255, 255, 255, 255};
+#include "sr_packet.h"
 
-void send_arp_packet(struct sr_instance*, uint8_t*, unsigned int, char*);
-void send_ip_packet(struct sr_instance*, uint8_t*, char*, uint16_t, uint16_t);
-
-void build_ether_header(uint8_t *_packet, uint8_t *addr, struct sr_if* interfaces, uint16_t protocol);
+void send_arp_packet(struct sr_instance* , uint8_t* , unsigned int , char* );
+void send_ip_error_packet(struct sr_instance* , uint8_t* , char* , uint16_t , uint16_t );
+void build_ether_header(uint8_t *, uint8_t *, struct sr_if* , uint16_t );
 void build_arp_header(uint8_t *, sr_arp_hdr_t* , struct sr_if* );
 void build_ip_header(uint8_t *, sr_ip_hdr_t* , struct sr_if* );
 void build_icmp_header(uint8_t *, uint8_t , uint8_t );
 void build_icmp_t3_header(uint8_t *, uint8_t *, sr_ip_hdr_t *, sr_icmp_hdr_t* , struct sr_if* , uint8_t , uint8_t );
-
 void send_icmp_echo_packet(struct sr_instance* , uint8_t* , unsigned int , char* );
+
+void send_arp_request(struct sr_instance *sr, uint32_t dst_ip, char *interface);
 
 #endif /* SR_MESSAGE_H */
