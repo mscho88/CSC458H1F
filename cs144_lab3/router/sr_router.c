de984bdb1a1c35b2350ccea897c96499a45594dc
minsu.cho@mail.utoronto.ca <minsu.cho@mail.utoronto.ca>
2014-10-17 20:28:42 -0400

    mininet

--- cs144_lab3/router/sr_router.c
+++ cs144_lab3/router/sr_router.c
@@ -248,7 +248,7 @@ void build_icmp_header(uint8_t *_packet, sr_icmp_hdr_t* icmp_header, struct sr_i
 
 	icmp_tmp_header->icmp_code = icmp_header->icmp_code;
 	if(icmp_header->icmp_type == icmp_protocol_type8){
-		icmp_tmp_header->icmp_type = icmp_protocol_type0;
+		icmp_tmp_header->icmp_type = icmp_protocol_type8;
 	}
 	icmp_tmp_header->icmp_sum = cksum((uint8_t*)icmp_header, (IPv4_MIN_LEN + 8 > temporary_len - ETHER_HEADER_LEN ? IPv4_MIN_LEN + 8 : temporary_len - ETHER_HEADER_LEN));
 	printf("icmp %u\n", icmp_tmp_header->icmp_sum);
