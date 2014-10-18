a3efeb7ea1b9e13bf8451c68a04ed7c7ff6c89ba
minsu.cho@mail.utoronto.ca <minsu.cho@mail.utoronto.ca>
2014-10-18 07:07:26 -0400

    mininet

--- cs144_lab3/router/sr_message.c
+++ cs144_lab3/router/sr_message.c
@@ -42,13 +42,14 @@ void send_ip_packet(struct sr_instance* sr, uint8_t* packet, char* interface, ui
 	sr_ip_hdr_t *ip_header = (sr_ip_hdr_t *)packet + sizeof(sr_ethernet_hdr_t);
 	sr_icmp_t3_hdr_t *icmp_header = (sr_icmp_t3_hdr_t *)packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_icmp_t3_hdr_t);
 
+	print_hdr_ip(ip_header);
 	build_ether_header(_packet, eth_header->ether_shost, interfaces);
 	build_ip_header(_packet + sizeof(sr_ethernet_hdr_t), ip_header, interfaces);
 	build_icmp_header(_packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_icmp_t3_hdr_t), packet, ip_header, icmp_header, interfaces, type, code);
 
-	print_hdr_eth(_packet);
+	/*print_hdr_eth(_packet);
 	print_hdr_ip(_packet + sizeof(sr_ethernet_hdr_t));
-	print_hdr_icmp(_packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_icmp_t3_hdr_t));
+	print_hdr_icmp(_packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_icmp_t3_hdr_t));*/
 	sr_send_packet(sr, (uint8_t *)_packet, length, interfaces->name);
 	free(_packet);
 }
