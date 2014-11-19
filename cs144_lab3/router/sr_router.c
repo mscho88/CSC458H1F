4a793a76be6d5d85ef022059405e376e295558fe
minsu.cho@mail.utoronto.ca <minsu.cho@mail.utoronto.ca>
2014-11-19 12:01:14 -0500

    fixing warning

--- cs144_lab3/router/sr_router.c
+++ cs144_lab3/router/sr_router.c
@@ -171,7 +171,8 @@ void sr_handlepacket_arp(struct sr_instance* sr,
 	}else if (ntohs(arp_hdr->ar_op) == arp_op_reply){
 		/* In case, the packet is the arp reply packet .. */
 		struct sr_arpreq *arp_packet;
-		if(*arp_packet = sr_arpcache_insert(&sr->cache, arp_hdr->ar_sha, arp_hdr->ar_sip) != NULL){
+		/****??? what the fuck is this warning ****/
+		if(&arp_packet = sr_arpcache_insert(&sr->cache, arp_hdr->ar_sha, arp_hdr->ar_sip) != NULL){
 			struct sr_packet *packets = arp_packet->packets;
 			while (packets != NULL) {
 				sr_ethernet_hdr_t *eth_hdr_2send = (sr_ethernet_hdr_t *)(packets->buf);
