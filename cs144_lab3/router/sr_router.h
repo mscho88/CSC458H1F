7027409079edfa89d5f1f912923e72b1c8642be7
minsu.cho@mail.utoronto.ca <minsu.cho@mail.utoronto.ca>
2014-11-26 02:36:22 -0500

    fixing a1

--- cs144_lab3/router/sr_router.h
+++ cs144_lab3/router/sr_router.h
@@ -73,7 +73,9 @@ void build_arp_header(uint8_t *, sr_arp_hdr_t* , struct sr_if* , uint16_t );
 void sr_handlepacket_arp(struct sr_instance* , uint8_t * , unsigned int , char* );
 void sr_handlepacket_ip(struct sr_instance* , uint8_t * , unsigned int , char* );
 struct sr_rt *sr_longest_prefix_match(struct sr_rt *, uint32_t);
-void sr_send_icmp_message(struct sr_instance *, uint8_t *, uint16_t , uint16_t );
+/*void sr_send_icmp_message(struct sr_instance *, uint8_t *, uint16_t , uint16_t );*/
+void sr_send_icmp_message(struct sr_instance *, uint8_t *, char *, uint16_t , uint16_t );
+
 void sr_arpcache_handle(struct sr_instance *, struct sr_arpreq *);
 void set_arp_hdr(uint8_t *, uint16_t , uint16_t , uint16_t ,
 		uint8_t *, uint32_t , uint8_t *, uint32_t );
