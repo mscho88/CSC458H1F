ee187ee613f399abaed8a17ffc29f4cb65330a3e
minsu.cho@mail.utoronto.ca <minsu.cho@mail.utoronto.ca>
2014-12-01 19:08:28 -0500

    INOUT bound

--- cs144_lab5/router/sr_router.c
+++ cs144_lab5/router/sr_router.c
@@ -235,6 +235,8 @@ void sr_nat_handle_icmp(struct sr_instance* sr,
 	if (strcmp(matching_ip->interface, OUTBOUND)){
 		/* If the packet is for outbound packet .. */
 		printf("It works \n");
+
+
 		mappings = sr_nat_lookup_internal(sr->nat, ip_hdr->ip_src, icmp_t3_hdr->unused, nat_mapping_icmp);
 		if(mappings == NULL){
 			mappings = sr_nat_insert_mapping(sr->nat, ip_hdr->ip_src, icmp_t3_hdr->unused, nat_mapping_icmp);
