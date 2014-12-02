40f84843974a0c7881fddbec1f754a6baaf6647d
minsu.cho@mail.utoronto.ca <minsu.cho@mail.utoronto.ca>
2014-12-02 04:25:49 -0500

    A2

--- cs144_lab5/router/sr_router.h
+++ cs144_lab5/router/sr_router.h
@@ -74,6 +74,8 @@ int sr_read_from_server(struct sr_instance* );
 /* -- sr_router.c -- */
 void sr_init(struct sr_instance* );
 void sr_handlepacket(struct sr_instance* , uint8_t * , unsigned int , char* );
+void sr_handlepacket_arp(struct sr_instance* , uint8_t * , unsigned int , char* );
+void sr_handlepacket_ip(struct sr_instance* , uint8_t * , unsigned int , char* );
 void sr_nat_translate(struct sr_instance*, uint8_t*, int,  struct sr_nat_mapping*, sr_nat_trans_type);
 char* sr_longest_prefix_match(struct sr_instance *sr, uint32_t );
 
