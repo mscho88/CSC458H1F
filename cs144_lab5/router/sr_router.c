7b692d5014bb280194a9076fb4a80b91a79c7e62
minsu.cho@mail.utoronto.ca <minsu.cho@mail.utoronto.ca>
2014-12-01 22:26:39 -0500

    INOUT bound

--- cs144_lab5/router/sr_router.c
+++ cs144_lab5/router/sr_router.c
@@ -80,10 +80,10 @@ void sr_handlepacket(struct sr_instance* sr,
     assert(interface);
 
 	/* Initialize NAT external IP if it is not already */
-    if(sr->nat.activated && sr->nat->nat_external_ip == 0)
+    if(sr->nat.activated && sr->nat.nat_external_ip == 0)
     {
         struct sr_if* natExternalInterface = sr_get_interface(sr, OUTBOUND);
-        sr->nat->nat_external_ip = natExternalInterface->ip;
+        sr->nat.nat_external_ip = natExternalInterface->ip;
     }
 
     print_hdrs(packet,len);
