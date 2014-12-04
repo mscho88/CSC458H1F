cef69ad9c7a3412f9e2966c48e5c1d8b95a33536
minsu.cho@mail.utoronto.ca <minsu.cho@mail.utoronto.ca>
2014-12-04 05:09:13 -0500

    a2

--- cs144_lab5/router/sr_router.c
+++ cs144_lab5/router/sr_router.c
@@ -478,7 +478,7 @@ void sr_send_icmp(struct sr_instance *sr, uint8_t *packet,
 	assert(packet);
 	assert(interface);
 
-    int length = type == icmp_type0 ? len : sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t) + sizeof(sr_icmp_hdr_t);
+    int length = type == icmp_type0 ? len : sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t) + sizeof(sr_icmp_t3_hdr_t);
 
     uint8_t *_packet = (uint8_t*) malloc(length);
 
@@ -511,7 +511,6 @@ void sr_send_icmp(struct sr_instance *sr, uint8_t *packet,
 
     /* build ICMP header */
     if(type == icmp_type0){
-        /*memcpy(_packet, packet, length);*/
         icmp_hdr_2send->unused    = icmp_hdr->unused;
         icmp_hdr_2send->next_mtu  = icmp_hdr->next_mtu;
         icmp_hdr_2send->icmp_type = type;
@@ -529,6 +528,7 @@ void sr_send_icmp(struct sr_instance *sr, uint8_t *packet,
     icmp_hdr_2send->icmp_sum  = 0;
     icmp_hdr_2send->icmp_sum  = cksum(icmp_hdr_2send, ntohs(ip_hdr_2send->ip_len) - sizeof(sr_ip_hdr_t));
 
+    printf("packet size = %d(%d)\n", sizeof(packet), len);
     print_hdr_eth(eth_hdr);
     print_hdr_eth(eth_hdr_2send);
 	print_hdr_ip(ip_hdr);
